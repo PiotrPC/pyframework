f32d239704fc973df1e5513275244e57d654e122
Piotr <pc58@interia.pl>
2023-11-20 21:30:20 +0100

    1.01

---
+++ ai_web/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=250)  # Pole tytuł
+    content = models.TextField(default='')
+    year = models.PositiveSmallIntegerField(default=2023)
+    imgThumb = models.ImageField(upload_to="images", null=True, blank=True)
+
+    def __str__(self):
+        return self.title_with_year()
+
+    def title_with_year(self):
+        return "{} ({})".format(self.title, self.year)
