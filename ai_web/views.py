9080486e9b1956a68942d21ed0ab73dee060cf03
Piotr <pc58@interia.pl>
2023-11-23 01:09:52 +0100

    1.21 Koniec

--- ai_web/views.py
+++ ai_web/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Article
 
 # Create your views here.
 def index(response):
     # return HttpResponse("Pierwszy widok")
-    return render(response, "articles.html")
+    title_article = "Salon Samochodowy"
+    articles = Article.objects.all()
+    options = ["opcja 1", "opcja 2"]
+
+    return render(response, "articles.html",
+                  {
+                      "title_article_view": title_article,
+                      "options": options,
+                      "articles": articles
+                  })
