3638a124a83726921ccf10fccb8b727007d6ca8a
Piotr <pc58@interia.pl>
2023-12-07 00:18:45 +0100

    1.41 Migracjedanych/Poprawki

--- ai_web/models.py
+++ ai_web/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=255)
     content = models.TextField(default='')
     year = models.PositiveSmallIntegerField(default=2023)
     imgThumb = models.ImageField(upload_to="images", null=True, blank=True)
